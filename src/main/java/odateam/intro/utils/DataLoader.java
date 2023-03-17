f309fd0b27341d8d0cb8e6b6bab02edcf046b36a
Nik Kovačević <nik.kovacevic@student.um.si>
2023-03-17 19:52:48 +0100

    data loading refactoring

--- src/main/java/odateam/intro/utils/DataLoader.java
+++ src/main/java/odateam/intro/utils/DataLoader.java
@@ -3,8 +3,8 @@ package odateam.intro.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import odateam.intro.model.Country;
 import odateam.intro.model.Rates;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,8 +14,12 @@ import java.util.Map;
 public class DataLoader {
 
     public static List<Country> loadRatesData() throws IOException {
-        ClassPathResource resource = new ClassPathResource("data.json");
-        byte[] jsonData = FileCopyUtils.copyToByteArray(resource.getInputStream());
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://euvatrates.com/rates.json";
+
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        String jsonData = response.getBody();
 
         ObjectMapper objectMapper = new ObjectMapper();
         Rates rates = objectMapper.readValue(jsonData, Rates.class);
